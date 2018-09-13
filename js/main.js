ff43fb324a7e980c6e0f142ba9b41a2c2c6fb065
Lucksya <generalsolid.7372@gmail.com>
2018-09-13 22:02:50 +0530

    Lots of Changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
 $(document).ready(function () { 
   // loader
   $(window).load(function(){
-    $('#loader').delay(400).fadeOut(500);
+    //$('#loader').delay(400).fadeOut(500);
+    //$('#content').delay(1000).css({'display':'contents'});
+    $('.loader-circle').delay(400).fadeOut(500);
+    $('.loader-line-mask').delay(400).fadeOut(500);
+    $('.loader-line').delay(400).fadeOut(500);
+    $('#continue').delay(400).fadeIn(500);
+    //$('.particles-js-canvas-el').css({'z-index':'0'});
   })
 })
+
+//Post Page Load Functions
+function removeLoader() {
+  //document.getElementById("section_loader").innerHTML = "Hello World";
+  $('#section_loader').delay(400).fadeOut(500);
+  $('#contents').delay(400).fadeIn(500);
+  $('#aboutIRIS').delay(400).fadeIn(500);
+  if(screen.width>=575){
+     $('#menu').delay(400).fadeIn(500);
+      $('#leftSideber').delay(400).fadeIn(500);
+  }
+  //$('#contentBody').delay(1000).css({'height':'2000px'});
+}
+
+// Scroll Functions
+$(window).bind("scroll", function() {
+  if ($(this).scrollTop() > 10) {
+        $(".navbar").css({'background-color':'rgba(51,51,51,1)'});
+    } else {
+        $(".navbar").css({'background-color':'rgba(51,51,51,0.8)'});
+    }
+    /*if ($(this).scrollTop() > 520) {
+        $("#contactUs").fadeIn();
+        $("#aboutIRIS").stop().fadeOut();
+    } else {
+        $("#contactUs").stop().fadeOut();
+        $("#aboutIRIS").fadeIn();
+    }*/
+});
--- js/particles.js
+++ js/particles.js
@@ -1508,6 +1508,10 @@ window.particlesJS = function(tag_id, params){
   /* set size canvas */
   canvas_el.style.width = "100%";
   canvas_el.style.height = "100%";
+  canvas_el.style.position="fixed";
+  canvas_el.style.top = "0%";
+  canvas_el.style.left = "0%";
+  
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
