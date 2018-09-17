584541039e7432d736b465ee95e416c077a13b2d
Lucksya <generalsolid.7372@gmail.com>
2018-09-17 22:57:56 +0530

    smooth scroll, fade in and outs

--- index.php
+++ index.php
@@ -21,6 +21,9 @@
 		<!-- Load an icon library -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+		<!-- CSS Libraries -->
+		<link rel="stylesheet" href="lib/animate.css">		
+
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -71,6 +74,7 @@
 				</div>
 			</section>
 			<!-- Loader -->
+			
 			<!-- Contents -->
 			<section>
 				<div id="content_body">
--- js/main.js
+++ js/main.js
@@ -6,20 +6,42 @@ $(document).ready(function () {
     $('.loader-circle').delay(400).fadeOut(500);
     $('.loader-line-mask').delay(400).fadeOut(500);
     $('.loader-line').delay(400).fadeOut(500);
-    $('#continue').delay(400).fadeIn(500);
+    $('#continue').delay(400).fadeIn(500).addClass("animated fadeInDown");
     //$('.particles-js-canvas-el').css({'z-index':'0'});
   })
+
+  //Smooth Scrolling
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
 });
 
 //Post Page Load Functions
 function removeLoader() {
   //document.getElementById("section_loader").innerHTML = "Hello World";
-  $('#section_loader').delay(400).fadeOut(500);
-  $('#contents').delay(400).fadeIn(500);
-  $('#aboutIRIS').delay(400).fadeIn(500);
-  //$('.vertical-centered-box').css({'overflow':'auto'})
+  $('#section_loader').delay(400).addClass("animated fadeOut").fadeOut(50);
+  $('#contents').delay(400).fadeIn(200).addClass("animated fadeInUpBig");
   if(screen.width>=575){
-     $('#menuLg').delay(400).fadeIn(500);
+     $('#menuLg').delay(400).fadeIn(200).addClass("animated fadeInDown");
      $('#leftSideber').delay(400).fadeIn(500);
   }else{
     $('#menuSm').delay(400).fadeIn(500);
