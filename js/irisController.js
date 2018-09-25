d4e6b84d9dd4abcf9481f39444abc477b649d94b
Lucksya <generalsolid.7372@gmail.com>
2018-09-25 18:22:48 +0530

    sports logos updated

--- js/irisController.js
+++ js/irisController.js
@@ -45,6 +45,7 @@ function irisController($scope, $http, $sce){
 	var getManagmentEvents=function(){
 		$http.get("php/getManagmentEvents.php")
    			.then(function (response) {
+   				console.log(response);
    				$scope.managmentEvents = response.data;
    				transformToHTML($scope.managmentEvents);
    			});
